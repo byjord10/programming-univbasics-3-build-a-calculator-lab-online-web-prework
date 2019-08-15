27039683a9ec87580382b3bd84feacd780d1f676
Brianna Jordan <bryvjordan9@gmail.com>
2019-08-15 20:21:22 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,22 +1,22 @@
 require 'spec_helper'
 
-def addition(num1=5,num2=4)
+def addition(num1,num2)
 sum = num1 + num2
 end
 
-def subtraction(num1=10, num2=5)
+def subtraction(num1, num2)
 sum = num1 - num2
 end
 
-def division(num1= 50, num2= 2)
+def division(num1, num2)
 sum = num1 / num2
 end
 
-def multiplication(num1 = 4, num2 = 30)
+def multiplication(num1, num2)
 sum = num1 * num2
 end
 
-def modulo(num1 = 34, num2 = 5)
+def modulo(num1, num2)
   sum = num1 % num2
 
 end
